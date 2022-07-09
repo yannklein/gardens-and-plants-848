6247fe26bb7b5ef152884031946455f3ed1c5a8e
Yann Klein <yann.klein@me.com>
2022-07-09 15:47:36 +0900

    added destroy to plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -10,6 +10,12 @@ class PlantsController < ApplicationController
     end
   end
 
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    redirect_to garden_path(@plant.garden)
+  end
+
   private
 
   def plant_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :gardens do
     resources :plants, only: [:create]
   end
-  
+  resources :plants, only: :destroy
 end
